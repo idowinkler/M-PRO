1d01a95f733f651315bb348da259fc90c752d9ae
DaniGamliel03 <dgamliel8@gmail.com>
2026-03-23 14:35:00 +0200

    style changes

--- src/components/OptimalPlanningModal/OptimalPlanningModal.tsx
+++ src/components/OptimalPlanningModal/OptimalPlanningModal.tsx
@@ -1,4 +1,3 @@
-
 import DataStockSection from "../DataStockSection/DataStockSection";
 import Modal from "../Modal/Modal";
 import styles from "./OptimalPlanningModal.module.css";
@@ -9,25 +8,26 @@ interface OptimalPlanningModalProps {
   onClose: () => void;
 }
 
-const OptimalPlanningModal: React.FC<OptimalPlanningModalProps> = ({ isOpen, onClose }) => {
-    const stocks: DataStockItem[] = [
-    { id: '1', name: 'מניה בעלת שווי ש"ח 1', },
-    { id: '2', name: 'מניה בעלת שווי ש"ח 2', },
-    { id: '3', name: 'מניה בעלת שווי ש"ח 3', },
+const OptimalPlanningModal: React.FC<OptimalPlanningModalProps> = ({
+  isOpen,
+  onClose,
+}) => {
+  const stocks: DataStockItem[] = [
+    { id: 1, name: "מאגר הכי מושלם שיש בעולם" },
+    { id: 2, name: "מאגר של מפת חום" },
+    { id: 3, name: "מאגר חמורים בעזה 🫏" },
     // ... more stocks
   ];
-  
-    return (
-        <Modal 
-            isOpen={isOpen}
-            onClose={onClose}
-            title="יצירת מפת חום"
-        >
-            <div className={styles.container}>
-                <DataStockSection dataStockItems={stocks}/>
-            </div>
-        </Modal>
-    )
-}
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} title="יצירת מפת חום">
+      <div className={styles.container}>
+        <div className={styles.divider}/>
+        <h4 className={styles.title}>בחירת מאגר נתונים</h4>
+        <DataStockSection dataStockItems={stocks} />
+      </div>
+    </Modal>
+  );
+};
 
 export default OptimalPlanningModal;
